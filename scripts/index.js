8d81c3523f30fa7122b5bc158dc2c73931c6394e
Anton Kiryushkin <rezvens@yandex.ru>
2024-07-12 21:13:32 +0300

    for => forEach

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,18 @@ function cardsAddContent(name, link) {
     cardContent.name = cardContent.querySelector('.card__title').textContent = name; // добавили свойству name имя карточки
     cardContent.link = cardContent.querySelector('.card__image').src = link; // добавили свойству link ссылку на карточку
 
-    const placesList = document.querySelector('.places__list'); // нашли контейнер для карточек
-    placesList.append(cardContent); // добавили карточку в конец контейнера
-
     const deleteButton = cardContent.querySelector('.card__delete-button'); //нашли кнопку удаления
     deleteButton.addEventListener('click', function () { //добавили слушатель для кнопки при клике
     const card = deleteButton.closest('.card'); //нашли ближайшего родителя card
     card.remove(); //удалили этот элемент
-    })};
+    })
+    return cardContent;
+  };
 
-function cardsRender() {
-  for (let i = 0; i < initialCards.length; i++) { // запустили цикл по длинне массива с карточками
-    cardsAddContent(initialCards[i].name, initialCards[i].link) // вызвали функцию, передали name и link
-}};
+  initialCards.forEach(function (card) {
+    cardContent = cardsAddContent(card.name, card.link) // вызвали функцию, передали name и link
+    const placesList = document.querySelector('.places__list'); // нашли контейнер для карточек
+    placesList.append(cardContent); // добавили карточку в конец контейнера
+  });
 
 cardsRender();
