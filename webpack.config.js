5d263670c3ee8d6ac337972357790e4605827a6c
kiryushkinav <kiryushkinav@srgroup.ru>
2024-07-29 20:01:37 +0300

    npm + webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = { // module.exports — это синтаксис экспорта в Node.js 
+  entry: { main: './src/index.js'},
+  output: { // указали, в какой файл будет собираться весь js, и дали ему имя 
+    path: path.resolve(__dirname, 'dist'), // переписали точку выхода, используя утилиту path 
+    filename: 'main.js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devtool: "eval-source-map",
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
+  module: {
+  rules: [ // rules — это массив правил
+    { // добавим в него объект правил для бабеля
+    test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+    use: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
+    exclude: '/node_modules/' // исключает папку node_modules, файлы в ней обрабатывать не нужно
+    }
+  ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
+}
